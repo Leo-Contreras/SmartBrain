7fcf94b6364301b83062d93d7d29de951af897c1
Leo-Contreras <leonardoaguie@gmail.com>
2021-07-20 15:29:23 -0700

    Uninstalling particles animation package

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -4,7 +4,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-import ParticleAnimation from 'react-particle-animation';
 import './App.css';
 
 
